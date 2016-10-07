8cde6d29613984472db7480ac5e935045f26da8f
over <overln2@gmail.com>
2016-10-07 13:14:39 +0600

    mplayer fix

--- lib/utils.py
+++ lib/utils.py
@@ -85,7 +85,7 @@ def get_video_duration(file):
                         time = timedelta(hours=hours, minutes=minutes, seconds=seconds)
                     break
         else:
-            run_mplayer = mplayer('-identify', '-frames', '0', file)
+            run_mplayer = mplayer('-identify', '-frames', '0', '-nosound',file)
             for line in run_mplayer.split('\n'):
                 if 'ID_LENGTH=' in line:
                     time = timedelta(seconds=int(float(line.split('=')[1])))
